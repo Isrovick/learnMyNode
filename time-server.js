a3d1b1af2b9a7fda76f67fa5bf1f2e083f6414c3
Israel Morales <Isrovick@gmail.com>
2022-02-08 23:21:22 -0400

    > finished time-server.js

---
+++ time-server.js
@@ -0,0 +1,42 @@
+var strftime = require('strftime')
+const net = require('net')  
+
+const [a,b,port, ...args]=process.argv;
+
+
+     const server = net.createServer(function (socket) {  
+        //socket.write(data)
+        //"2013-07-06 17:42" 
+        socket.end(strftime('%F %H:%M')+"\n")
+     }) 
+
+server.listen(port)
+
+/*
+SUGGESTED SOLUTION
+
+
+    'use strict'
+    const net = require('net')
+    
+    function zeroFill (i) {
+      return (i < 10 ? '0' : '') + i
+    }
+    
+    function now () {
+      const d = new Date()
+      return d.getFullYear() + '-' +
+        zeroFill(d.getMonth() + 1) + '-' +
+        zeroFill(d.getDate()) + ' ' +
+        zeroFill(d.getHours()) + ':' +
+        zeroFill(d.getMinutes())
+    }
+    
+    const server = net.createServer(function (socket) {
+      socket.end(now() + '\n')
+    })
+    
+    server.listen(Number(process.argv[2]))
+
+
+*/
